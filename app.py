68b2e5c6f6ea32aa8365bade1ba735d72ae4c620
rvmodi-bit <rvmodi@bluevalleyk12.net>
2026-04-19 21:17:24 -0500

    added pages
    added pages

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("DTG: Dermatology resource guide!!")
+st.subheader("Navigate through the next pages to see all cool stuff")
+st.write("This is not professional advice, this is by a 7th grader 😅")
--- pages/Fitz_Survey.py
+++ pages/Fitz_Survey.py
@@ -19,16 +19,33 @@ burn_intes = st.selectbox("When you go out in the sun for too long how bad if yo
 tan_number = {"Instant tan": 0, "Easily tanned": 1, "Tanned in an average time": 2, "Long time to tan":3, "No tan": 4}
 tan_intes = st.selectbox("Skin color", ["Instant tan", "Easily tanned", "Tanned in an average time", "Long time to tan", "No tan"])
 
+freckles_number = {"A lot of freckles": 0, "High amount of freckles": 1, "Moderate freckles": 2, "Partial freckles": 3, "No freckles": 4}
+freckles_amount = st.select_slider("Select the amount of freckles you have",
+                                   options = ["A lot of freckles", "High amount of freckles", "Moderate freckles", "Partial freckles", "No freckles"]
+                                   )
+
 before_score += eye_number[eye_color]
 before_score += hair_number[hair_color]
 before_score += skin_number[skin_color]
 before_score += burn_number[burn_intes]
 before_score += tan_number[tan_intes]
+before_score += freckles_number[freckles_amount]
+
+if -1 < before_score < 5:
+    fpscale = 1
+if 4 < before_score < 9:
+    fpscale = 2
+if 8 < before_score < 13:
+    fpscale = 3
+if 12 < before_score < 17:
+    fpscale = 4
+if 16 < before_score < 21:
+    fpscale = 5
+if 20 < before_score < 25:
+    fpscale = 6
 
 st.header("Results")
-
-
-st.subheader(f"Your Fitzpatrick score is {before_score}/20")
-
+st.subheader(f"Your Fitzpatrick score is {before_score}/24")
+st.write(f"That means that you're {fpscale} on the Fitzpatrick scale")
 
 
