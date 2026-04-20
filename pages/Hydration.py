fbb8bda1e25e49a741f176b33baa984964c4d1c5
rvmodi-bit <rvmodi@bluevalleyk12.net>
2026-04-19 22:28:53 -0500

    sawd

--- pages/Hydration.py
+++ pages/Hydration.py
@@ -1,3 +1,24 @@
 import streamlit as st
 
-st.slider()
+facial_stage = ""
+st.subheader("Water intake")
+water = st.slider("How many cups of water do you drink in a day?", 0, 20, 0)
+st.write("---")
+st.subheader("Coffee intake")
+caffine = st.slider("How many cups of coffee did you have today?", 0, 15, 0)
+
+percentage = water * 12.5 - caffine * 10
+
+st.subheader(f"Your facial hydration for today is {percentage}%")
+if caffine > 3 :
+    if percentage > 80 :
+        st.write(":rainbow[Even though your hydration is good... DONT DRINK THAT MUCH COFFEE!!]")
+
+if percentage < 70 :
+    facial_stage = "Dehydrated"
+if 70 < percentage < 100 :
+    facial_stage = "Bueno 👌"
+if 110 < percentage :
+    facial_stage = "Plump"
+
+st.write(f"Face quality: {facial_stage}")
